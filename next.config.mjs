b3028b2ba68ee75c6100bcd5f780cd77ec51aa05
codewithxavi <javieradrian.it@gmail.com>
2024-09-11 19:30:11 +0200

    Continue commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['starwars-visualguide.com'],
     },
+    basePath: '/starwars',
+    assetPrefix: '/starwars/',
+    trailingSlash: true,
+    output: 'export', // Add this line
 }
 
 export default nextConfig
